94fb8923b2a30819ab7d32670be741c50d0563af
Kirill Krasikov <krasikov@protonmail.com>
2020-07-31 13:08:17 +0700

    added generate mcts games scrip

--- dlgo/encoders/base.py
+++ dlgo/encoders/base.py
@@ -25,6 +25,6 @@ def get_encoder_by_name(name, board_size):
     if isinstance(board_size, int):
         board_size = (board_size, board_size)
 
-    module = importlib.import_module('dlgo.encoders' + name)
+    module = importlib.import_module('dlgo.encoders.' + name)
     constructor = getattr(module, 'create')
-    return constructor
+    return constructor(board_size)
