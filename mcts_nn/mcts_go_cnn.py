eb63b9284f2f15a042df96110a10bccbed074b8b
Kirill Krasikov <krasikov@protonmail.com>
2020-08-10 10:19:46 +0700

    added mcts based nn models

---
+++ mcts_nn/mcts_go_cnn.py
@@ -0,0 +1,54 @@
+from __future__ import print_function
+
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPooling2D
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+
+np.random.seed(123)
+X = np.load('../generated_games/features-200.npy')
+Y = np.load('../generated_games/labels-200.npy')
+
+samples = X.shape[0]
+size = 9
+input_shape = (size, size, 1)
+
+X = X.reshape(samples, size, size, 1)
+
+train_samples = int(0.9 * samples)
+X_train, X_test = X[:train_samples], X[train_samples:]
+Y_train, Y_test = Y[:train_samples], Y[train_samples:]
+
+model = Sequential()
+model.add(Conv2D(48, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(Dropout(rate=0.5))
+model.add(Conv2D(48, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.5))
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(size * size, activation='softmax'))
+model.summary()
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='sgd',
+              metrics=['accuracy'])
+
+model.fit(X_train, Y_train,
+          batch_size=64,
+          epochs=100,
+          verbose=1,
+          validation_data=(X_test, Y_test))
+
+score = model.evaluate(X_test, Y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
